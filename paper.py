eafeafc479669ab85747f14f93da8958d07ce337
Roman Gor <roman.gor.home@gmail.com>
2023-03-30 16:04:58 +0300

    multi symbol feature

--- paper.py
+++ paper.py
@@ -15,6 +15,7 @@ from prettytable import PrettyTable
 from requests import ReadTimeout
 
 load_dotenv()
+ENV = os.getenv('ENV') or 'local'
 
 os.environ['TZ'] = 'UTC'
 event_log = 'event.csv'
@@ -27,7 +28,11 @@ INCREASE_SHORT = 'increase_short'
 DIRECTION_LONG = 'Long'
 DIRECTION_SHORT = 'Short'
 
-balance = starting_balance = 500
+if len(sys.argv) > 1:
+    balance = starting_balance = float(sys.argv[1])
+else:
+    balance = starting_balance = 500
+
 low_risk_per_trade = .02
 high_risk_per_trade = .1
 
@@ -48,6 +53,7 @@ ema2_amplitude = 2.5
 ema3_length = 50
 ema3_amplitude = 2.5
 
+symbol_position = None
 long_position = False
 short_position = False
 entry_price = 0
@@ -64,29 +70,31 @@ wins = 0
 loses = 0
 trailing_loses = 0
 
-symbol = 'APTUSDT'
+df = {}
+symbols = ['APTUSDT', 'DYDXUSDT', 'ETCUSDT', 'MATICUSDT', 'ATOMUSDT', 'OPUSDT', 'IMXUSDT', 'AVAXUSDT']
 interval = Client.KLINE_INTERVAL_1MINUTE
 start_time = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d 00:00:00')  # Yesterday time
 end_time = time.strftime('%Y-%m-%d %H:%M:%S')  # Current time
 
 
 def print_log(data, pt=None):
-    telegram_text = 'Env: ' + os.getenv('ENV') + '\n'
+    telegram_text = 'Env: ' + ENV + '\n'
 
     if pt is None:
         pt = PrettyTable(['Param', 'Value'])
         for key, value in data.items():
             pt.add_row([key, value])
             telegram_text += str(key) + ': ' + str(value) + '\n'
-        pt.add_row(['Env', os.getenv('ENV')])
+        pt.add_row(['Env', ENV])
 
     print(pt)
     sys.stdout.flush()
 
-    send_text = 'https://api.telegram.org/bot' + os.getenv('TELEGRAM_BOT_ID') + '/sendMessage?chat_id=' \
-                + str(os.getenv('TELEGRAM_CHAT_ID')) + '&parse_mode=html&text=' + telegram_text
+    if os.getenv('TELEGRAM_CHAT_ID') and os.getenv('TELEGRAM_BOT_ID'):
+        send_text = 'https://api.telegram.org/bot' + os.getenv('TELEGRAM_BOT_ID') + '/sendMessage?chat_id=' \
+                    + str(os.getenv('TELEGRAM_CHAT_ID')) + '&parse_mode=html&text=' + telegram_text
 
-    response = requests.get(send_text)
+        response = requests.get(send_text)
 
 
 def create_client():
@@ -140,10 +148,10 @@ def get_dataframe(s, i, st, et):
     return local_df
 
 
-def fix_dataframe_index():
-    df["ema1"] = ema(df["close"], length=ema1_length)
-    df["ema2"] = ema(df["close"], length=ema2_length)
-    df["ema3"] = ema(df["close"], length=ema3_length)
+def fix_dataframe_index(s):
+    df[s]["ema1"] = ema(df[s]["close"], length=ema1_length)
+    df[s]["ema2"] = ema(df[s]["close"], length=ema2_length)
+    df[s]["ema3"] = ema(df[s]["close"], length=ema3_length)
 
     # df["ema1_amplitude"] = get_percentage_difference(df["close"], df["ema1"])
     # df["ema2_amplitude"] = get_percentage_difference(df["close"], df["ema2"])
@@ -168,10 +176,11 @@ def dump_to_csv(event_data):
         event_data.to_csv(event_log, index=False, header=False, mode='a')
 
 
-def close_position(pnl, direction):
+def close_position(s, pnl, direction):
     global long_position, short_position, trailing_loses, loses, wins, \
-        touches, position_size, asset_size, balance
+        touches, position_size, asset_size, balance, symbol_position
 
+    symbol_position = None
     if direction is DIRECTION_LONG:
         long_position = False
     else:
@@ -192,7 +201,7 @@ def close_position(pnl, direction):
     balance += pnl
 
 
-def manage_opened_position(current_price, direction):
+def manage_opened_position(s, current_price, direction):
     global stop_loss_price, take_profit_price, touches, long_position, trailing_loses, loses, position_size, \
         asset_size, balance, entry_price, wins, short_position
 
@@ -207,7 +216,7 @@ def manage_opened_position(current_price, direction):
     pnl = calculate_pnl(exit_price, direction is DIRECTION_SHORT)
 
     if pnl < 0:  # stop loss
-        close_position(pnl, direction)
+        close_position(s, pnl, direction)
     else:  # take profits
         if touches <= max_trailing_takes:  # trailing
             last_action_increase = INCREASE_LONG if direction is DIRECTION_LONG else INCREASE_SHORT
@@ -231,6 +240,7 @@ def manage_opened_position(current_price, direction):
                 take_profit_price = entry_price * (1 - trailing_take_profit)
 
             print_log({
+                'Symbol': s,
                 'Time': current_time,
                 f'Increase {direction}': touches - 1,
                 'Position Size': f'{position_size:,.2f}',
@@ -242,9 +252,10 @@ def manage_opened_position(current_price, direction):
 
             return
         else:  # absolute take profit
-            close_position(pnl, direction)
+            close_position(s, pnl, direction)
 
     print_log({
+        'Symbol': s,
         'Time': current_time,
         'Close': f'{direction} 🔵️',
         'Pnl': f'${pnl:,.2f}',
@@ -254,9 +265,9 @@ def manage_opened_position(current_price, direction):
     })
 
 
-def process_kline_event(df_data, event_data):
+def process_kline_event(s, df_data, event_data):
     global balance, trend, long_position, stop_loss_price, take_profit_price, touches, trailing_loses, loses, wins, \
-        last_orders, entry_price, short_position, position_size, last_action, trades, asset_size
+        last_orders, entry_price, short_position, position_size, last_action, trades, asset_size, symbol_position
 
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
 
@@ -272,16 +283,20 @@ def process_kline_event(df_data, event_data):
     current_price = float(event_data.close)
     actual_amplitude = get_percentage_difference(current_price, df_data.ema2)
 
-    # print(f"Current price: {current_price}, Ema1: {df_data.ema1}, Ema2: {df_data.ema2}, Ema3: {df_data.ema3}, Actual "
+    # print(f"Symbol: {s}, Current price: {current_price}, Ema1: {df_data.ema1}, Ema2: {df_data.ema2}, Ema3: {df_data.ema3}, Actual "
     #       f"Amplitude: {actual_amplitude}")
 
     # reason to long exit
-    if long_position and (current_price <= stop_loss_price or current_price >= take_profit_price):
-        manage_opened_position(current_price, DIRECTION_LONG)
+    if long_position \
+            and s == symbol_position \
+            and (current_price <= stop_loss_price or current_price >= take_profit_price):
+        manage_opened_position(s, current_price, DIRECTION_LONG)
         return
 
-    if short_position and (current_price >= stop_loss_price or current_price <= take_profit_price):
-        manage_opened_position(current_price, DIRECTION_SHORT)
+    if short_position \
+            and s == symbol_position \
+            and (current_price >= stop_loss_price or current_price <= take_profit_price):
+        manage_opened_position(s, current_price, DIRECTION_SHORT)
         return
 
     if not short_position and touches == 0 \
@@ -290,6 +305,7 @@ def process_kline_event(df_data, event_data):
             and current_price > df_data.ema3 \
             and actual_amplitude > 0 \
             and abs(actual_amplitude) >= ema1_amplitude:
+        symbol_position = s
         entry_price = current_price
         position_size = calculate_entry_position_size()
         asset_size = position_size / entry_price
@@ -302,12 +318,14 @@ def process_kline_event(df_data, event_data):
         last_orders = []
         last_orders.append(
             {
+                'symbol': s,
                 'entry_price': entry_price,
                 'last_action': last_action
             }
         )
 
         print_log({
+            'Symbol': s,
             'Time': current_time,
             'Open': 'Short 🔴',
             'Position Size': f'${position_size:,.2f}',
@@ -324,6 +342,7 @@ def process_kline_event(df_data, event_data):
             and current_price < df_data.ema3 \
             and actual_amplitude < 0 \
             and abs(actual_amplitude) >= ema1_amplitude:
+        symbol_position = s
         entry_price = current_price
         position_size = calculate_entry_position_size()
         asset_size = position_size / entry_price
@@ -336,12 +355,14 @@ def process_kline_event(df_data, event_data):
         last_orders = []
         last_orders.append(
             {
+                'symbol': s,
                 'entry_price': entry_price,
                 'last_action': last_action
             }
         )
 
         print_log({
+            'Symbol': s,
             'Time': current_time,
             'Open': 'Long 🟢',
             'Position Size': f'${position_size:,.2f}',
@@ -355,6 +376,8 @@ def process_kline_event(df_data, event_data):
 
 
 def handle_socket_message(event):
+    s = event['ps']
+
     event_df = pd.DataFrame([event['k']])
     event_df = event_df.set_axis([
         'kline_start_time', 'kline_close_time', 'interval', 'first_trade_id', 'last_trade_id', 'open', 'close',
@@ -364,11 +387,11 @@ def handle_socket_message(event):
     event_df.drop('interval', axis=1, inplace=True)
     event_df.astype(float)
 
-    df_data = df.iloc[-1]
+    df_data = df[s].iloc[-1]
     event_data = event_df.iloc[0]
 
     # dump_to_csv(event_df)
-    process_kline_event(df_data, event_data)
+    process_kline_event(s, df_data, event_data)
 
 
 def update_dataframe(skip_timer=False):
@@ -380,42 +403,26 @@ def update_dataframe(skip_timer=False):
     previous_minute = (datetime.now() - timedelta(minutes=1)).strftime('%Y-%m-%d %H:%M:%S')
     current_minute = time.strftime('%Y-%m-%d %H:%M:%S')
 
-    try:
-        fresh_df = get_dataframe(symbol, interval, previous_minute, current_minute)
+    for symbol in symbols:
+        try:
+            fresh_df = get_dataframe(symbol, interval, previous_minute, current_minute)
 
-        index = fresh_df.first_valid_index()
-        if index in df.index:
-            df.drop(index, inplace=True)
+            index = fresh_df.first_valid_index()
+            if index in df[symbol].index:
+                df[symbol].drop(index, inplace=True)
 
-        df = pd.concat([df, fresh_df])
-        fix_dataframe_index()
-    except ReadTimeout as re:
-        print(f"Time: {current_minute}, Exception ❗ Type: ReadTimeout")
-    except BinanceAPIException as bae:
-        print(f"Time: {current_minute}, Exception ❗ Type: BinanceAPIException, Message: f{bae.message}")
+            df[symbol] = pd.concat([df[symbol], fresh_df])
+            fix_dataframe_index(symbol)
+        except ReadTimeout as re:
+            print(f"Time: {current_minute}, Exception ❗ Type: ReadTimeout")
+        except BinanceAPIException as bae:
+            print(f"Time: {current_minute}, Exception ❗ Type: BinanceAPIException, Message: f{bae.message}")
 
-        client = create_client()
+            client = create_client()
 
-
-t = PrettyTable(['Param', 'Value'])
-t.add_row(['Start Time', start_time])
-t.add_row(['End Time', end_time])
-t.add_row(['Interval', interval])
-t.add_row(['Low Risk Per Trade', f"{low_risk_per_trade * 100}%"])
-t.add_row(['High Risk Per Trade', f"{high_risk_per_trade * 100}%"])
-t.add_row(['Leverage', leverage])
-t.add_row(['Stop Loss', f"{stop_loss * 100}%"])
-t.add_row(['Take Profit', f"{take_profit * 100}%"])
-t.add_row(['Trailing Stop Loss', f"{trailing_stop_loss * 100}%"])
-t.add_row(['Trailing Take Profit', f"{trailing_take_profit * 100}%"])
-t.add_row(['Max Trailing Take Profit', max_trailing_takes])
-# t.add_row(['EMA Length', ema_length])
-# t.add_row(['EMA Amplitude', ema_amplitude])
-# t.add_row(['RSI Length', rsi_length])
-# t.add_row(['RSI Long Reason', rsi_long_reason])
-# t.add_row(['RSI Short Reason', rsi_short_reason])
 
 print_log({
+    'Balance': f'${balance:,.2f}',
     'Start Time': start_time,
     'End Time': end_time,
     'Interval': interval,
@@ -431,8 +438,9 @@ print_log({
 
 client = create_client()
 
-df = get_dataframe(symbol, interval, start_time, end_time)
-fix_dataframe_index()
+for symbol in symbols:
+    df[symbol] = get_dataframe(symbol, interval, start_time, end_time)
+    fix_dataframe_index(symbol)
 
 while True:
     now = time.localtime().tm_sec
@@ -445,5 +453,6 @@ while True:
 twm = ThreadedWebsocketManager()
 twm.start()
 
-twm.start_kline_futures_socket(callback=handle_socket_message, symbol=symbol)
+for symbol in symbols:
+    twm.start_kline_futures_socket(callback=handle_socket_message, symbol=symbol)
 twm.join()
